b99fce48460fc46316515ff9f95dedbbab16892c
DoveyLoveyCora <aidongyang@vsulv.com>
2020-12-31 19:02:07 +0800

    添加代码提交检查+stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
   extends: ['stylelint-config-recommended', 'stylelint-config-standard'],
   rules: {
-    "indentation": 2
+    "indentation": 2,
+    "no-descending-specificity": null,
+    "selector-pseudo-class-no-unknown": null,
+    "property-no-unknown": null
   }
 }
